14d6b12c667da05331d0f95192f5baac19931d93
prabinthapa10 <prabinmagar636@gmail.com>
2025-04-17 10:25:29 +0545

    NAVBAR

---
+++ Kitab-Ghar/src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+  faUser,
+  faSearch,
+  faHeart,
+  faShoppingCart,
+} from "@fortawesome/free-solid-svg-icons";
+
+function Navbar() {
+  return (
+    <div class="bg-white shadow px-6 py-4 flex items-center justify-between">
+      {/*  Left: Logo */}
+      <div className="flex items-center space-x-2">
+        <img src="/images/logo.png" alt="Kitab Ghar Logo" class="w-8 h-8" />
+        <span class="text-xl font-bold border border-purple-500 px-2 rounded">
+          Kitab Ghar
+        </span>
+      </div>
+      <ul class="hidden md:flex space-x-6 text-sm font-medium text-black">
+        <li>Home</li>
+        <li>Book</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* right icons */}
+      <div className="flex items-center space-x-4 text-xl">
+        <a href="#">
+          <FontAwesomeIcon icon={faUser} />
+        </a>
+        <a href="#">
+          <FontAwesomeIcon icon={faSearch} />
+        </a>
+        <a href="#">
+          <FontAwesomeIcon icon={faHeart} />
+        </a>
+        <a href="#">
+          <FontAwesomeIcon icon={faShoppingCart} />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
