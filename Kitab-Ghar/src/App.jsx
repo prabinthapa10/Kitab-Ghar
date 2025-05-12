329fa03a80c1c101d7602367091aa18258bc3cde
amangrx <ransingaman@gmail.com>
2025-05-12 14:17:28 +0545

    Implementation of protected and public routes

--- Kitab-Ghar/src/App.jsx
+++ Kitab-Ghar/src/App.jsx
@@ -14,25 +14,81 @@ import { ToastContainer } from "react-toastify";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import BookDetail from "./pages/Admin/BookDetail";
+import PublicRoutes from "./routes/PublicRoutes";
+import ProtectedRoutes from "./routes/ProtectedRoutes";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Signup />} />
-          <Route path="/book" element={<Book />} />
-          <Route path="/book/:id" element={<BookDescriptionPage />} />
-          <Route path="/user" element={<UserDetails />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
+          <Route
+            path="/"
+            element={
+              <PublicRoutes restricted>
+                <Home />
+              </PublicRoutes>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <PublicRoutes restricted>
+                <Login />
+              </PublicRoutes>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <PublicRoutes restricted>
+                <Signup />
+              </PublicRoutes>
+            }
+          />
+          <Route
+            path="/book"
+            element={
+              <PublicRoutes restricted>
+                <Book />
+              </PublicRoutes>
+            }
+          />
+          <Route
+            path="/book/:id"
+            element={
+              <PublicRoutes restricted>
+                <BookDescriptionPage />
+              </PublicRoutes>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <PublicRoutes restricted>
+                <AboutPage />
+              </PublicRoutes>
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <PublicRoutes restricted>
+                <ContactPage />
+              </PublicRoutes>
+            }
+          />
 
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/admin/addBook" element={<AddBook />} />
-          <Route path="/admin/bookDetails" element={<BookDetail />} />
+          <Route element={<ProtectedRoutes roles={["Member"]} />}>
+            <Route path="/user" element={<UserDetails />} />
+            <Route path="/cart" element={<CartPage />} />
+          </Route>
+
+          <Route element={<ProtectedRoutes roles={["Admin"]} />}>
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/admin/addBook" element={<AddBook />} />
+            <Route path="/admin/bookDetails" element={<BookDetail />} />
+          </Route>
 
           {/* <Route path="/book_card" element={<BookCard />} /> */}
           {/* <Route path="/book/" element={<BookList />} /> */}
